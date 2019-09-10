9f6af20a4414f23c4ef6a53ac4fbe2b9c3b44188
Arunesh Mishra <aruneshm@gmail.com>
2019-09-10 15:54:03 -0700

    animation

---
+++ one.js
@@ -0,0 +1,8 @@
+$('.card-header').on('click', function() {
+
+  $('.card-body').slideToggle(300, 'swing');
+  $('#card-one form').slideToggle(300, 'swing');
+  //$('.chat-message-counter').fadeToggle(300, 'swing');
+
+});
+
