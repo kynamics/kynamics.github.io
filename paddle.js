8304277599e85e646310c43772be6b8e9fc25963
Arunesh Mishra <aruneshm@gmail.com>
2019-10-29 20:13:19 -0700

    paddle ball game bricks

--- paddle.js
+++ paddle.js
@@ -6,6 +6,17 @@ var Game = /** @class */ (function () {
         this.gctx = this.gcanvas.getContext('2d');
     }
     Game.prototype.sayHello = function () {
+        console.log("Html canvas height = " + this.gcanvas.height + ", width = " + this.gcanvas.width);
+        for (var i = 0; i < 10; i++) {
+            var x = Math.round(Math.random() * this.gcanvas.width / 2);
+            var y = Math.round(Math.random() * this.gcanvas.height / 2);
+            var width = Math.round(Math.random() * this.gcanvas.width / 2);
+            var height = Math.round(Math.random() * this.gcanvas.height / 2);
+            this.gctx.strokeStyle = 'rgb(0, ' + Math.floor(x % 255) + ', ' +
+                Math.floor(y % 255) + ')';
+            this.gctx.strokeRect(x, y, width, height);
+            console.log(" x=" + x + " y = " + y + " width = " + width + " height = " + height);
+        }
         this.gctx.fillStyle = 'green';
         this.gctx.fillRect(10, 10, 50, 50);
         return "Hello " + this.name;
--- paddle.ts
+++ paddle.ts
@@ -2,6 +2,11 @@
 
 class Game {
 
+    public static readonly BRICK_WIDTH = 25;
+    public static readonly BRICK_HEIGHT = 10;
+    public static readonly GUTTER_X = 4;
+    public static readonly GUTTER_Y = 2;
+
     name: string;
     gcanvas: HTMLCanvasElement;
     gctx: CanvasRenderingContext2D;
@@ -13,12 +18,36 @@ class Game {
     }
 
     sayHello() {
+        console.log("Html canvas height = " + this.gcanvas.height + ", width = " + this.gcanvas.width);
+        this.drawBricks();
+
+        /* for (let i = 0; i < 10; i ++) {
+            let x: number = Math.round(Math.random() * this.gcanvas.width / 2);
+            let y: number = Math.round(Math.random() * this.gcanvas.height / 2);
+            let width: number = Math.round(Math.random() * this.gcanvas.width / 2);
+            let height: number = Math.round(Math.random() * this.gcanvas.height / 2);
+            this.gctx.strokeStyle = 'rgb(0, ' + Math.floor(x % 255) + ', ' + 
+                         Math.floor(y % 255) + ')';
+            this.gctx.strokeRect(x, y, width, height);
+            console.log(" x=" + x + " y = " + y + " width = " + width + " height = " + height);
+        } */
         this.gctx.fillStyle = 'green';
         this.gctx.fillRect(10, 10, 50, 50);
         return "Hello " + this.name;
     }
-}
 
+    drawBricks() {
+        this.gctx.strokeStyle  = 'rgb(255,10,10)';
+        for (let i = 0; i < 20; i ++) {
+            for (let j = 0; i < 5; j ++) {
+                let x: number = i * Game.BRICK_WIDTH + Game.GUTTER_X;
+                let y: number = j * Game.BRICK_HEIGHT + Game.GUTTER_Y;
+                this.gctx.strokeRect(x, y, Game.BRICK_WIDTH, Game.BRICK_HEIGHT);
+            }
+        }
+
+    }
+}
 
 let myGame = new Game("This is a test game");
 console.log(" what does the game say: " + myGame.sayHello());
